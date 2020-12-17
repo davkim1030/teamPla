c42402d0b7967e4fffd170aacee7ca1d5aa19d36
min <minalee0696@gmail.com>
2020-12-17 15:06:56 +0900

    samin

--- teamPla/apis.py
+++ teamPla/apis.py
@@ -1,2 +1,36 @@
-from teamPla.models import Project
+from models import *
 
+
+"""
+42api 권환 획득
+42api 사용법을 알아야한다.
+"""
+def login():
+	pass
+
+"""
+팀이 등록되었는지 확인
+"""
+def isteamMatched():
+	pass
+"""
+팀 매칭 신청 (프로젝트 등록)
+"""
+def reister(project):
+	pass
+
+"""
+과제종료
+team의 exitVote에 1추가
+"""
+def voteExit():
+	pass
+"""
+로그아웃
+"""
+def logout():
+	pass
+
+
+def readById(user):
+	Team.
--- teamPla/models.py
+++ teamPla/models.py
@@ -1,5 +1,9 @@
+from enum import Enum
 from django.db import models
 
+class Auth(Enum):
+	USER = 1
+	ADMIN = 2
 
 class Project(models.Model):
     """
@@ -9,3 +13,20 @@ class Project(models.Model):
     """
     name = models.CharField(max_length=25, primary_key=True)
     recommend_day = models.IntegerField(null=False)
+
+class Team(models.Model):
+	pass
+
+
+class User(models.Model):
+	"""
+    42Cursus 프로젝트에 관한 클래스
+    :intraId 유저의 로그인 Id
+    :team 테이블의 외래키
+	:auth 유저의 권한
+	:project 푸로제트 관련 정보
+    """
+	intraId = models.CharField(max_length=10, primary_key=True)
+	team = models.ForeignKey(Team, on_delete=models.CASCADE)
+	auth = models.CharField(max_length=1, choices = Auth)
+	project = models.ForeignKey(Project, on_delete=models.CASCADE)
