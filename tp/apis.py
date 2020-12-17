07a660492fa53a6cfa85a77504458807b25ebe46
saint <jkl@DESKTOP-PKMAN31.localdomain>
2020-12-17 21:31:21 +0900

    team_exit 0 string debug

--- tp/apis.py
+++ tp/apis.py
@@ -1,7 +1,7 @@
-from models import *
+from .models import *
 from datetime import datetime
-
 import random
+
 
 def team_update(team: Team, **kwargs):
     for var, value in kwargs:
@@ -9,14 +9,15 @@ def team_update(team: Team, **kwargs):
 
 
 def team_exit(team: Team):
-    if 0 not in team.exitVote:
-        target_list = Team.objects.filter(id = team.id)
-    user_list = target_list[0].userList.split(seq = ',')
+    if "0" not in team.exitVote:
+        target_list = Team.objects.filter(id=team.id)
+    user_list = target_list[0].userList.split(seq=',')
     for user_id in user_list:
-        users = User.objects.filter(intraId = user_id)
+        users = User.objects.filter(intraId=user_id)
         for user in users:
             user.objects.update(Project=None)
     Team.objects.delete(id = target_list[0].id)
+
 
 def team_match():
     for prj in Project.objects.all():
@@ -30,44 +31,9 @@ def team_match():
             member_list = list()
             for i in range(3):
                 member_list.append(team_list.pop())
-            Team.objects.create(userList = ','.join(member_list), exitVote = "0000", project = prj, dueDate = datetime.date.today().strftime("%Y%m%d"))
+            Team.objects.create(userList=','.join(member_list), exitVote="0000", project=prj, dueDate=datetime.date.today().strftime("%Y%m%d"))
             user_nb -= 3
         if len(team_list):
-            Team.objects.create(userList = ','.join(team_list), exitVote = "0000", project = prj, dueDate = datetime.date.today().strftime("%Y%m%d"))
+            Team.objects.create(userList=','.join(team_list), exitVote="0000", project=prj, dueDate=datetime.date.today().strftime("%Y%m%d"))
         del team_list
 
-
-
-"""
-42api 권환 획득
-42api 사용법을 알아야한다.
-"""
-def login():
-	pass
-
-"""
-팀이 등록되었는지 확인
-"""
-def isteamMatched():
-	pass
-"""
-팀 매칭 신청 (프로젝트 등록)
-"""
-def reister(project):
-	pass
-
-"""
-과제종료
-team의 exitVote에 1추가
-"""
-def voteExit():
-	pass
-"""
-로그아웃
-"""
-def logout():
-	pass
-
-
-def readById(user):
-	Team.
